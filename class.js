482a31c4f38361b0a457d6a965d7f6e3357624a8
eriudo <30628964+eriudo@users.noreply.github.com>
2019-09-09 10:55:56 -0300

    Add files via upload

---
+++ class.js
@@ -0,0 +1,57 @@
+class question{
+	constructor(id, items, itemCorreto){
+	this.id = id;
+	this.items = items;
+	this.itemCorreto = itemCorreto;
+}
+	criar(){
+
+	}
+	buscar(){
+
+	}
+	consultar{
+
+	}
+	editar(){
+
+	}
+	remover(){
+		
+	}
+
+}
+
+class saveFile{
+	constructor()
+	this.;
+	this.;
+}
+
+class readFile{
+	constructor(){
+	this.;
+	this.;
+}
+}
+
+class register{
+	constructor(){
+	this.as;
+	this.;
+}
+}
+
+class configuration{
+	constructor(){
+	this.;
+	this.;
+}
+}
+
+class test{
+	constructor(){
+	this.;
+	this.;
+}
+}
