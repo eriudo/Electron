26787ec2d376f01b643c3e86624a89161d81dd77
eriudo <30628964+eriudo@users.noreply.github.com>
2019-09-13 14:56:18 -0300

    Add files via upload

--- class.js
+++ class.js
@@ -65,18 +65,24 @@ class register{
 	this.descricao = descricao;
 	this.sexo = sexo;
 }
-	novo(id, items){
-		//tenho que pegar o arquivo, e tirar uma questao de la
+	var novo = new register(this.nome, this.dataNascimento, this.descricao, this.sexo);
+	//preciso salvar as novas pessoas num arquivo JSON
+	
 
-	}
-	buscar(){
+	buscar(id){
+		if (id == this.id){
+			//vai mostrar alguma coisa, o teste da pessoa
+
+		} else {
+			console.log ("nao existe esse id");
+		}
 
 	}
 	consultar{
-		//consultar uma questao especifica pra saber a % de acerto
+		//consultar e buscar mesma coisa?
 	}
 	editar(){
-		//
+		
 
 	}
 	remover(){
@@ -93,8 +99,19 @@ class configuration{
 }
 
 class test{
-	constructor(){
-	this.;
-	this.;
+	constructor(data,diagnostico,observacao,audio,fonte,ordem,aleatorio){
+	this.data = data;
+	this.diagnostico = diagnostico;
+	this.observacao = observacao;
+	this.audio = audio;
+	this.fonte = fonte;
+	this.ordem = ordem;
+	this.aleatorio = aleatorio;
 }
+
+
+
+
+
+
 }
